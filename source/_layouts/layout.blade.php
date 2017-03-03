011da17cac2667feb0e513d0c1393695da330f8d
Mark Redeman <markredeman@gmail.com>
2017-03-03 22:42:37 +0100

    Use relative paths for javascript files

--- source/_layouts/layout.blade.php
+++ source/_layouts/layout.blade.php
@@ -27,7 +27,7 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"> </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="/js/Menu.js"></script>
-        <script src="/js/Register.js"></script>
+        <script src="./js/Menu.js"></script>
+        <script src="./js/Register.js"></script>
     </body>
 </html>
