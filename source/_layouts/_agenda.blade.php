f8fc5332c73053fec46a239a9cbd5c7a41d31eb4
Mark Redeman <markredeman@gmail.com>
2018-02-05 21:38:36 +0100

    Update location

--- source/_layouts/_agenda.blade.php
+++ source/_layouts/_agenda.blade.php
@@ -1,9 +1,9 @@
 <div class="agenda p-0">
 	<div class="agenda-body" style="padding: 3em; padding-bottom: 0">
-		<a href="https://www.openstreetmap.org/?mlat=53.22203&amp;mlon=6.56388#map=18/53.22203/6.56388">
+		<a href="https://www.openstreetmap.org/?mlat=53.21945&amp;mlon=6.56455#map=19/53.21945/6.56455">
 			<h3 class="section-header agenda-header">
 				<i class="fa fa-map-marker mr-2" aria-hidden="true"></i>
-				Het Heerenhuis
+        Van Swinderen Huys
 			</h3>
 		</a>
 	</div>
@@ -16,7 +16,7 @@
         scrolling="no"
         marginheight="0"
         marginwidth="0"
-        src="http://www.openstreetmap.org/export/embed.html?bbox=6.560339927673341%2C53.22058479205571%2C6.567420959472657%2C53.22347216544705&amp;layer=mapnik&amp;marker=53.22202850308373%2C6.563880443572998"
+        src="http://www.openstreetmap.org/export/embed.html?bbox=6.562775373458863%2C53.21870901916789%2C6.566315889358521%2C53.220181700530986&amp;layer=mapnik&amp;marker=53.219445366178725%2C6.564545631408691"
 		    style="
                max-width: 100%;
                border: 2px solid #eee;
